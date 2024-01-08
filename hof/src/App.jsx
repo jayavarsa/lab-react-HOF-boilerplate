862f11aee2a409f579feb2693dc6347dff271419
jayavarsan <jayavarsan.r@kalvium.community>
2024-01-08 10:28:00 +0530

    Your commit message

---
+++ hof/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import HigherOrderFunction from "./components/HigherOrderFunction";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HigherOrderFunction />
+      </div>
+    </>
+  );
+}
+
+export default App;
